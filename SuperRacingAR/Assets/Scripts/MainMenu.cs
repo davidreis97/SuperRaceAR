427b0ff7df8729b586aa1b1a3a5700da0e563279
ruimoliveira <ruimiguelalmeidaoliveira@gmail.com>
2019-10-23 14:46:00 +0100

    inits menu functions

---
+++ SuperRacingAR/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+
+    /*
+     * Buttons' Functions
+     * */
+    public void Play()
+    {
+
+    }
+
+    public void EditCar()
+    {
+
+    }
+
+    public void howToPLay()
+    {
+
+    }
+
+    public void About()
+    {
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
