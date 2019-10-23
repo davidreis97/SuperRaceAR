9f86e7eef403a64f33eb0cc1105b0a4c38549217
João Barbosa <bmpj13@hotmail.com>
2019-10-23 16:37:22 +0100

    Car movement engine done

--- SuperRacingAR/Assets/Scripts/CarMovement.cs
+++ SuperRacingAR/Assets/Scripts/CarMovement.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class CarMovement : MonoBehaviour
 {
-    public float speed = 30f;
-    public float turnSpeed = 2f;
+    public float speed = 1f;
+    public float turnSpeed = 10f;
+    private float originalSpeed;
+    private float originalTurnSpeed;
     private float powerInput;
     private float turnInput;
     private Rigidbody carRigidbody;
 
     void Awake()
     {
+        originalSpeed = speed;
+        originalTurnSpeed = turnSpeed;
         carRigidbody = GetComponent<Rigidbody>();
+        carRigidbody.maxAngularVelocity = 10;
     }
 
     // Update is called once per frame
@@ -24,7 +29,20 @@ public class CarMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        carRigidbody.AddRelativeForce(0f, 0f, powerInput * speed);
+        transform.Translate(Vector3.back * speed * Time.deltaTime);
         carRigidbody.AddRelativeTorque(0f, turnInput * turnSpeed, 0f);
+    }
+
+    public void slowDownSpeed()
+    {
+        speed = speed * 0.2f;
+        turnSpeed = turnSpeed * 0.5f;
+
+    }
+
+    public void resetSpeed()
+    {
+        speed = originalSpeed;
+        turnSpeed = originalTurnSpeed;
     }
 }
---
+++ SuperRacingAR/Assets/Scripts/OutOfMapDetector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfMapDetector : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        CarMovement cm = (CarMovement)other.gameObject.GetComponent("CarMovement");
+        cm.slowDownSpeed();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        CarMovement cm = (CarMovement)other.gameObject.GetComponent("CarMovement");
+        cm.resetSpeed();
+    }
+}
