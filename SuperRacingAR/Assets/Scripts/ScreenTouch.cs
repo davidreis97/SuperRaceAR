aa9c7e40ec5360c558e2a1d6ce71a24eb8f79b98
David Reis <david.alex.reis.97@gmail.com>
2019-10-23 19:03:01 +0100

    Car now turns with touch Created prefab for car

--- SuperRacingAR/Assets/Scripts/ScreenTouch.cs
+++ SuperRacingAR/Assets/Scripts/ScreenTouch.cs
@@ -9,28 +9,38 @@ public class ScreenTouch : MonoBehaviour
     {
         
     }
-
+    int IsRightOrLeft(float x){
+        if(x > (Screen.width / 2)){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
     // Update is called once per frame
-    void Update()
+    public float GetInput()
     {
         int rightOrLeft = 0;
 
         for(int i = 0; i < Input.touchCount; i++){
             Touch touch = Input.GetTouch(i);
-
-            if(touch.position.x > (Screen.width / 2)){
-                rightOrLeft++;
-            }else{
-                rightOrLeft--;
-            }
+            
+            rightOrLeft += IsRightOrLeft(touch.position.x);
         }
 
+        if(Application.isEditor){
+            if(Input.GetMouseButton(0)){ //Hack to make it work on the simulator as well
+                Debug.Log("Here");
+                rightOrLeft += IsRightOrLeft(Input.mousePosition.x);
+            }
+        }
+        
+
         if(rightOrLeft > 0){
-            Debug.Log("Going right");
+            return 1.0f;
         }else if(rightOrLeft < 0){
-            Debug.Log("Going left");
+            return -1.0f;
         }else{ //if(rightOrLeft == 0){
-            Debug.Log("Going straight");
+            return 0.0f;
         }
     }
 }
