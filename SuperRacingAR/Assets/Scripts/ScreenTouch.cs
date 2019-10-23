afd8ed6152ccd0ba84b3e826ce39dad12e93c610
David Reis <david.alex.reis.97@gmail.com>
2019-10-23 18:34:55 +0100

    Added script which tracks touch events

---
+++ SuperRacingAR/Assets/Scripts/ScreenTouch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenTouch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int rightOrLeft = 0;
+
+        for(int i = 0; i < Input.touchCount; i++){
+            Touch touch = Input.GetTouch(i);
+
+            if(touch.position.x > (Screen.width / 2)){
+                rightOrLeft++;
+            }else{
+                rightOrLeft--;
+            }
+        }
+
+        if(rightOrLeft > 0){
+            Debug.Log("Going right");
+        }else if(rightOrLeft < 0){
+            Debug.Log("Going left");
+        }else{ //if(rightOrLeft == 0){
+            Debug.Log("Going straight");
+        }
+    }
+}
