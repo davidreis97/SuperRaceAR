ae289a3c3a0eb95f8d5e7abef828689b0845f390
ruimoliveira <ruimiguelalmeidaoliveira@gmail.com>
2019-10-24 15:58:53 +0100

    fixes some settings

--- SuperRacingAR/Assets/Scripts/MainMenu.cs
+++ SuperRacingAR/Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -28,7 +29,7 @@ public class MainMenu : MonoBehaviour
 
     public void EditCar()
     {
-
+        SceneManager.LoadScene("EditCar");
     }
 
     public void howToPLay()
