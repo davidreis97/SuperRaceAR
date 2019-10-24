9aee19e53ba180f3b3b9a8ba1ab6a849b20da1a2
David Reis <david.alex.reis.97@gmail.com>
2019-10-24 12:42:00 +0100

    Added dynamic lighting

---
+++ SuperRacingAR/Assets/LightCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightCalculator : MonoBehaviour
+{
+    LightsBySensor lbs;
+    Light spotlight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lbs = GameObject.Find("LightController").GetComponent<LightsBySensor>();
+        spotlight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lbs != null && lbs.LightLevel > 0){
+            if(spotlight != null){
+                spotlight.intensity = 1 - ((float) lbs.LightLevel/500f);
+            }
+        }
+    }
+}
