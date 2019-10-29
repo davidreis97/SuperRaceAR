3f32e0a10fe0d773027ad67e773030a35e3cc6cb
ruimoliveira <ruimiguelalmeidaoliveira@gmail.com>
2019-10-29 15:51:33 +0000

    adds UI to game scene

--- SuperRacingAR/Assets/Scripts/MainMenu.cs
+++ SuperRacingAR/Assets/Scripts/MainMenu.cs
@@ -5,13 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    /*
-    // Start is called before the first frame update
+    
     void Start()
     {
         
     }
 
+    /*
     // Update is called once per frame
     void Update()
     {
